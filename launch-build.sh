b061c08bda19814e24952116c8479c690d4f8b32
jcoquerygithub <32167067+jcoquerygithub@users.noreply.github.com>
2024-02-01 21:19:12 +0100

    Adjust shell from initial Dockerfile

--- launch-build.sh
+++ launch-build.sh
@@ -9,7 +9,7 @@ make -C mpy-cross
 cd ../picotool
 mkdir build && cd build && export PICO_SDK_PATH=/pico/micropython/lib/pico-sdk && cmake ../ && make
 export PATH=/pico/picotool/build:$PATH
-cd ../micropython
+cd ../micropython/ports/rp2
 sed -i 's/#define MICROPY_HW_ENABLE_UART_REPL.*/#define MICROPY_HW_ENABLE_UART_REPL (1) \/\/ useful if there is no USB/' mpconfigport.h
 make
 cp -r micropython/ports/rp2/build-RPI_PICO /output
